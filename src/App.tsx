33666c3b63b74847aea5cf79ba6be3eea0f9a378
boceksoft_ofis <huseyin.topuz35@gmail.com>
2024-08-05 13:24:03 +0300

    installed class-variance-authority

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
-     <p>Hello World</p>
-    </>
-  )
+    <div className="w-full max-h-screen flex flex-col px-4 py-2">
+      <Header />
+      <div className="w-full flex items-center justify-between">
+        <div>Sidebar</div>
+        <div>Content</div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { Menu, Search } from "lucide-react";
+import logo from "../assets/youtube.svg";
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between gap-4 lg:gap-10">
+      <div className="flex items-center space-x-4">
+        <Menu />
+        <img src={logo} alt="youtube" className="h-8 w-8" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <div className="border flex items-center">
+          <input placeholder="Search" />
+          <Search color="gray" size={18} />
+        </div>
+        <div></div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
