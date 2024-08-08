0553178688345f26d57d29268e9f0d52dc3911d4
boceksoft_ofis <huseyin.topuz35@gmail.com>
2024-08-09 00:24:41 +0300

    custom button oluşturuldu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import { Menu, Search } from "lucide-react";
 import logo from "../assets/youtube.svg";
+import Button from "./Button";
 const Header = () => {
   return (
     <div className="flex items-center justify-between gap-4 lg:gap-10">
       <div className="flex items-center space-x-4">
-        <Menu />
+        <Button>
+          <Menu />
+        </Button>
         <img src={logo} alt="youtube" className="h-8 w-8" />
       </div>
       <div className="flex items-center space-x-4">
