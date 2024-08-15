a5710aa9663d92ddd53859da249f6c71b24f16c3
boceksoft_ofis <huseyin.topuz35@gmail.com>
2024-08-15 23:15:24 +0300

    category pills added

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,16 @@ function App() {
   return (
     <div className="w-full max-h-screen flex flex-col px-4 py-2">
       <Header />
-      <div className="grid grid-cols-[auto, 1fr] flex-grow-1 overflow-auto">
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto mt-4">
         <div>Sidebar</div>
-        <div className="sticky top-0 bg-white z-10 pb-4">
-          <CategoryPills
-            categories={categories}
-            selectedCategory={selectedCategory}
-            onSelect={setSelectedCategory}
-          />
+        <div className="overflow-x-hidden px-8 pb-4">
+          <div className="sticky top-0 bg-white z-10 pb-4">
+            <CategoryPills
+              categories={categories}
+              selectedCategory={selectedCategory}
+              onSelect={setSelectedCategory}
+            />
+          </div>
         </div>
       </div>
     </div>
