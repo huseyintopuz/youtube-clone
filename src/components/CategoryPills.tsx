ab326db64f14b61a7ced7bc8286d023f19cb8a57
boceksoft_ofis <huseyin.topuz35@gmail.com>
2024-08-19 22:48:08 +0300

    added functionality on horizontal scrolling

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,5 +1,6 @@
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import Button from "./Button";
+import { useEffect, useRef, useState } from "react";
 
 type CategoryPillProps = {
   categories: string[];
@@ -11,9 +12,56 @@ const CategoryPills = ({
   selectedCategory,
   onSelect,
 }: CategoryPillProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [translate, setTranslate] = useState(0);
+  const [isLeftVisible, setIsLeftVisible] = useState(false);
+  const [isRightVisible, setIsRightVisible] = useState(true);
+
+  const TRANSLATE_AMOUNT = 300;
+  const handleLeftClick = () => {
+    setTranslate((translate) => {
+      const newTranslate = translate - TRANSLATE_AMOUNT;
+      if (newTranslate <= 0) return 0;
+      return newTranslate;
+    });
+  };
+
+  const handleRightClick = () => {
+    setTranslate((translate) => {
+      if (containerRef.current === null) translate;
+
+      const newTranslate = translate + TRANSLATE_AMOUNT;
+      const edge = containerRef.current?.scrollWidth ?? 0;
+      const width = containerRef.current?.clientWidth ?? 0;
+
+      console.log(edge, width, newTranslate);
+
+      if (newTranslate + width >= edge) {
+        return edge - width;
+      }
+      return newTranslate;
+    });
+  };
+
+  useEffect(() => {
+    if (containerRef.current === null) return;
+    const observer = new ResizeObserver((entries) => {
+      const constainer = entries[0].target;
+      if (constainer === null) return;
+      setIsLeftVisible(translate > 0);
+      setIsRightVisible(
+        translate + constainer.clientWidth < constainer.scrollWidth
+      );
+    });
+    observer.observe(containerRef.current);
+    return () => observer.disconnect();
+  }, [translate, categories]);
   return (
-    <div className="overflow-x-hidden relative">
-      <div className="flex gap-3 whitespace-nowrap transition-transform w-[max-content]">
+    <div ref={containerRef} className="overflow-x-hidden relative">
+      <div
+        className="flex gap-3 whitespace-nowrap transition-transform w-[max-content]"
+        style={{ transform: `translateX(-${translate}px)` }}
+      >
         {categories.map((category) => (
           <Button
             key={category}
@@ -25,18 +73,36 @@ const CategoryPills = ({
           </Button>
         ))}
       </div>
-      <div
-        className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r
+      {isLeftVisible && (
+        <div
+          className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r
        from-white from-50% to-transparent w-24 h-full"
-      >
-        <Button
-          variant="ghost"
-          size="icon"
-          className="h-full aspect-square w-auto p-1.5"
         >
-          <ChevronLeft />
-        </Button>
-      </div>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="h-full aspect-square w-auto p-1.5"
+            onClick={handleLeftClick}
+          >
+            <ChevronLeft />
+          </Button>
+        </div>
+      )}
+      {isRightVisible && (
+        <div
+          className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l
+       from-white from-50% to-transparent w-24 h-full flex justify-end"
+        >
+          <Button
+            variant="ghost"
+            size="icon"
+            className="h-full aspect-square w-auto p-1.5"
+            onClick={handleRightClick}
+          >
+            <ChevronRight />
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
